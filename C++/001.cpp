7dee6ceae9f0a722c1aa5264099db06b9e2c7382
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-02-19 13:30:58 +0530

    Basic setup

---
+++ C++/001.cpp
@@ -0,0 +1,10 @@
+//Compile => g++ .\filename.cpp or filename.cpp+tab
+//Run => .\a.exe  or a+tab
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello world";
+    return 0;
+}
---
+++ Python/001.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = [None] * 5
+print(a)
+
+b = [5] * 5
+print(b)
+
+c = np.array([1,2,3,4,5])
+print(c)
